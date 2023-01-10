ac021cbe57ecc3e5c0d8d6fcc4f2dbd1a0bd7750
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-01-10 13:04:01 +0100

    "added swagger configuration"

---
+++ src/main/java/com/twinkles/talkamout/config/SwaggerConfigClass.java
@@ -0,0 +1,82 @@
+package com.twinkles.talkamout.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfigClass implements WebMvcConfigurer {
+
+
+    @Bean
+    public Docket SwaggerConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiDetails())
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.twinkles.talkamout"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry
+                .addResourceHandler("swagger-ui/")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private SecurityReference bearerAuthReference() {
+        return new SecurityReference("Bearer", new AuthorizationScope[0]);
+    }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Bearer", authorizationScopes));
+    }
+
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "Talk Am Out API",
+                "API for connecting therapists to patients",
+                "1.0",
+                "Free to use",
+                new springfox.documentation.service.Contact("", "http://twinkles.com", "adeyinkawale13@gmail.com"),
+                "API License",
+                "",
+                Collections.emptyList()
+        );
+    }
+
+}
