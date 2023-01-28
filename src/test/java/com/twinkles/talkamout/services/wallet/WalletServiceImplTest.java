338fa9176cb531030bc31c034197f97ad887ad01
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-01-28 15:04:30 +0100

    "updated wallet service test"

--- src/test/java/com/twinkles/talkamout/services/wallet/WalletServiceImplTest.java
+++ src/test/java/com/twinkles/talkamout/services/wallet/WalletServiceImplTest.java
@@ -1,14 +1,20 @@
 package com.twinkles.talkamout.services.wallet;
 
+import com.twinkles.talkamout.repository.WalletRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class WalletServiceImplTest {
+    private WalletService walletService;
+    @Mock
+    private WalletRepository walletRepository;
 
     @BeforeEach
     void setUp() {
+        walletService = new WalletServiceImpl(walletRepository);
     }
 }
